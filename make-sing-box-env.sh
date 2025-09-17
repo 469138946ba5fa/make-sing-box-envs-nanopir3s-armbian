2a77ddc030953e75f42a474f16d63eda371dca0d
469138946ba5fa <206320576+469138946ba5fa@users.noreply.github.com>
2025-09-18 00:57:19 +0800

    Update make-sing-box-env.sh
          ## 7. 删除 tun-in inbound，并同步清理 route.rules 里的引用
          #| .inbounds |= map(select(.tag != "tun-in"))
          #| .route.rules |= map(
          #    if .inbound? and (.inbound | type == "array") then
          #      .inbound |= map(select(. != "tun-in"))
          #    else
          #      .
          #    end
          #)

--- make-sing-box-env.sh
+++ make-sing-box-env.sh
@@ -349,6 +349,15 @@ if [ -f '${SING_BOX_FILE}' ]; then
           .
         end
       ))
+      ## 7. 删除 tun-in inbound，并同步清理 route.rules 里的引用
+      #| .inbounds |= map(select(.tag != "tun-in"))
+      #| .route.rules |= map(
+      #    if .inbound? and (.inbound | type == "array") then
+      #      .inbound |= map(select(. != "tun-in"))
+      #    else
+      #      .
+      #    end
+      #)
     ' '${SING_BOX_FILE}' > '${SING_BOX_FILE}.tmp' && mv -fv '${SING_BOX_FILE}.tmp' '${SING_BOX_FILE}'
 else
   echo "Error: ${SING_BOX_FILE} is not exist. Exiting."
